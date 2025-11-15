e84257b6027d4d17a52a9be9907f2b2cdfd9f090
Joao Prado <Joaovictorpachecoleite@gmail.com>
2025-11-15 20:04:05 -0300

    feat: implementa insercao e impressao basica do vetor

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Vetor novoVetor = new Vetor(4);
+        novoVetor.insere(10);
+        novoVetor.insere(10);
+        novoVetor.imprime();
+    }
+}
--- Vetor.java
+++ Vetor.java
@@ -2,8 +2,23 @@ public class Vetor {
     private int[] vetor;
     private int tamanhoAtualDoVetor;
 
-    public Vetor(int capacidadeMaximaDoVetor) { 
-        this.vetor = new int[capacidadeMaximaDoVetor]; 
-        this.tamanhoAtualDoVetor = 0; 
+    public Vetor(int capacidadeMaximaDoVetor) {
+        this.vetor = new int[capacidadeMaximaDoVetor];
+        this.tamanhoAtualDoVetor = 0;
+    }
+
+    public void insere(int numero) {
+        if (vetor.length == tamanhoAtualDoVetor) {
+            System.out.println("Erro: Vetor está cheio!");
+        } else {
+            vetor[tamanhoAtualDoVetor] = numero;
+            tamanhoAtualDoVetor++;
+        }
+    }
+
+    public void imprime() {
+        for (int i = 0; i < tamanhoAtualDoVetor; i++) {
+            System.out.println(vetor[i]);
+        }
     }
 }
