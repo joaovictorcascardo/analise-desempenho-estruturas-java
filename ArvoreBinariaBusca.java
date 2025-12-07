aa6e58b5b40b11f7f9c66a7b3fce8efa6da5f5d7
Joao Prado <Joaovictorpachecoleite@gmail.com>
2025-12-07 01:11:55 -0300

    feat: implementa estrutura de Arvore Binaria de Busca (insercao e busca)

---
+++ ArvoreBinariaBusca.java
@@ -0,0 +1,50 @@
+public class ArvoreBinariaBusca {
+    private No raiz;
+
+    private class No {
+        int valor;
+        No esquerda, direita;
+
+        public No(int valor) {
+            this.valor = valor;
+            this.esquerda = null;
+            this.direita = null;
+        }
+    }
+
+    public void insere(int valor) {
+        raiz = inserirRecursivo(raiz, valor);
+    }
+
+    private No inserirRecursivo(No atual, int valor) {
+        if (atual == null) {
+            return new No(valor);
+        }
+
+        if (valor < atual.valor) {
+            atual.esquerda = inserirRecursivo(atual.esquerda, valor);
+        } else if (valor > atual.valor) {
+            atual.direita = inserirRecursivo(atual.direita, valor);
+        }
+        return atual;
+    }
+
+    public boolean busca(int valor) {
+        return buscarRecursivo(raiz, valor);
+    }
+
+    private boolean buscarRecursivo(No atual, int valor) {
+        if (atual == null) {
+            return false;
+        }
+        if (valor == atual.valor) {
+            return true;
+        }
+
+        if (valor < atual.valor) {
+            return buscarRecursivo(atual.esquerda, valor);
+        } else {
+            return buscarRecursivo(atual.direita, valor);
+        }
+    }
+}
